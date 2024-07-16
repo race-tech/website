4254373ee945932b66109ba01d3fcac02ef91d14
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-07-16 18:02:42 -0400

    feat: changed favicon and edited landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,17 @@
 <div class="hero min-h-[calc(100vh-4.1rem)]" style="background-image: url(sf24_miami.jpg);">
 	<div class="hero-overlay bg-opacity-60"></div>
-	<div class="hero-content w-full justify-start text-neutral-content text-center">
-		<div class="max-w-md">
-			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-			<p class="mb-5">
-				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-				quasi. In deleniti eaque aut repudiandae et a id nisi.
-			</p>
-			<button class="group btn btn-primary">
-				Get Started
+	<div class="hero-content w-full justify-start text-neutral-content text-left">
+		<div class="max-w-lg">
+			<h1 class="mb-10 text-7xl font-bold">The historical motorsport database</h1>
+			<button
+				class="group transition-colors btn text-primary hover:text-primary-content btn-lg glass btn-wide btn-primary text-xl"
+			>
+				Get started now!
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6 group-hover:translate-x-2 group-hover:scale-110 transition-transform"
-					fill="#000"
 					viewBox="0 0 24 24"
+					fill="currentColor"
 					stroke="currentColor"
 					><path
 						d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"
